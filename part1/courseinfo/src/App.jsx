bc3c5f8ac34e510488b780e256a69a814304b4d5
Gabriella Mondvay <gabriellamondvay@gmail.com>
2024-01-03 21:50:09 +0100

    Fix bad merge

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,58 +1,29 @@
-const Header = (props) => {
-  console.log(props)
-  return (
-    <>
-      <h1>{props.course.name}</h1>
-    </>
-  )
-}
+const Header = ({ course }) => <h1>{course.name}</h1>
 
-const Part = (props) => {
-  console.log(props)
-  return (
-    <h1>{course}</h1>
-  )
-}
+const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
 
-const Content = ( {parts} ) => {
-  return (
-    <div>
-      <Part part={parts[0]} />
-      <Part part={parts[1]} />
-      <Part part={parts[2]} />
+const Content = ({ parts }) => (
+  <div>
+    <Part part={parts[0]} />
+    <Part part={parts[1]} />
+    <Part part={parts[2]} />
   </div>
-  )
-}
+)
 
-const Total = ({ parts }) => {
-  return (
-    <p>
-      Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}
-    </p>
-  )
-}
+const Total = ({ parts }) => (
+  <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+)
 
 const App = () => {
   const course = {
     name: 'Half Stack application development',
-    parts:
-      [
-  {
-    name: 'Fundamentals of React',
-    exercises: 10
-  },
-  {
-    name: 'Using props to pass data',
-    exercises: 7
-  },
-  {
-    name: 'State of a component',
-    exercises: 14
-  }
+    parts: [
+      { name: 'Fundamentals of React', exercises: 10 },
+      { name: 'Using props to pass data', exercises: 7 },
+      { name: 'State of a component', exercises: 14 }
     ]
   }
-  
-  
+
   return (
     <div>
       <Header course={course} />
