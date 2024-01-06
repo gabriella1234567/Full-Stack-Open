d626276058ecf580280a761ada3c315bcff2a503
Gabriella Mondvay <gabriellamondvay@gmail.com>
2024-01-06 15:32:46 +0100

    Finish Conditional rendering

--- part1/practice/complex-state-debugging/src/App.jsx
+++ part1/practice/complex-state-debugging/src/App.jsx
@@ -1,4 +1,29 @@
+/* eslint-disable react/prop-types */
 import { useState } from 'react'
+
+// Create History component to render click history only when allClicks array length is greater than 0
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+// Create Button component
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
 
 const App = () => {
   const [left, setLeft] = useState(0)
@@ -25,15 +50,11 @@ const App = () => {
   return (
     <div>
       {left}
-      <button onClick={handleLeftClick}>
-        left
-      </button>
-      <button onClick={handleRightClick}>
-        right
-      </button>
+      <Button handleClick={handleLeftClick} text="left" />
+      <Button handleClick={handleRightClick} text="right" />
       {right}
-      <p>{allClicks.join(' ')}</p>
-      <p>total: {total}</p>
+      <History allClicks={allClicks} />
+      <p>total clicks: {total}</p>
     </div>
   )
 }
